98e56f88e2631c265d535c8cda04a844c81c8dc8
Nur -E-Makshud <nure.makshud@hpl.com.bd>
2021-01-22 12:32:47 +0600

    go to hell

--- assignment.js
+++ assignment.js
@@ -1,9 +1,15 @@
 // https://github.com/makshud-nur/fundamental_JS
+/**
+if kmeter>0; than do the function else...
+ */
 function kilometerToMeter(kmeter){
     const mtr = kmeter * 1000; //multiply with 100
     return mtr;
 }
 //console.log(kilometerToMeter(3));
+/**
+if watch>0; phone>0 and laptop>0 than do the function else...
+ */
 function budgetCalculator(watch, phone, laptop){
     const watchCost = watch * 50;
     const phoneCost = phone * 100;
@@ -11,11 +17,9 @@ function budgetCalculator(watch, phone, laptop){
     const totalBudget = watchCost + phoneCost + laptopCost;
     return totalBudget;
 }
-//console.log(budgetCalculator(2, 4, 1));
+//console.log(budgetCalculator(2, 4, 0));
+
 /**
-function budgetCalculator(watch, phone, laptop){
-    50, 100, 500;
-}
 function hotelCost(yourDaysAtHotel){
     first 10 days-100
     11-20 80 dollar
