34f37f4c09398bb760d7804f2dbdabac02b32e6c
Nur -E-Makshud <nure.makshud@hpl.com.bd>
2021-01-22 11:48:09 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,18 @@
+//find factorial of n using recursive function
+function fact(n){
+    if(n == 0){
+        return 1;
+    }
+    else{
+        return n * fact(n-1); /*each time this factorial function is called with a parameter, it'll go look for the function and that's how it goes until n==0 */
+    }
+}
+var sum=fact(4);
+console.log(sum); 
+
+function kilometerToMeter(kmeter){
+    const mtr = kmeter * 1000;
+    return mtr;
+}
+var kmtometer = kilometerToMeter(13);
+console.log(kmtometer);
---
+++ inchToFeet.js
@@ -0,0 +1,7 @@
+function inchToFeet(inch){//variable inch is declared here...
+    var feet = inch/12;
+    return feet;
+}
+var NanaFeet = inchToFeet(156);
+console.log(NanaFeet);
+console.log(inchToFeet(288));
