f070a9a5737204c787e3e9b94a40d8268f9ddb3a
Nur -E-Makshud <nure.makshud@hpl.com.bd>
2021-01-22 11:55:00 +0600

    what

--- assignment.js
+++ assignment.js
@@ -1,15 +1,4 @@
-//find factorial of n using recursive function
-function fact(n){
-    if(n == 0){
-        return 1;
-    }
-    else{
-        return n * fact(n-1); /*each time this factorial function is called with a parameter, it'll go look for the function and that's how it goes until n==0 */
-    }
-}
-var sum=fact(4);
-console.log(sum); 
-
+// https://github.com/makshud-nur/fundamental_JS
 function kilometerToMeter(kmeter){
     const mtr = kmeter * 1000;
     return mtr;
